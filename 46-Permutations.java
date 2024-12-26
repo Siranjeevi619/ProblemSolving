3c0f2260c7f7046112b34dac470928ea14305f93
Siranjeevi <164711059+Siranjeevi619@users.noreply.github.com>
2024-12-26 09:35:59 +0530

    [46] [Time Beats: 100.00%] [Memory Beats: 33.19%] - LeetPush

--- 46-Permutations.java
+++ 46-Permutations.java
@@ -1,29 +1,27 @@
 class Solution {
-    static void permutate(List<List<Integer>> result , int index, int [] nums){
-        if(index == nums.length){
-            // result.add(new ArrayList<>());
-            List<Integer> current = new ArrayList<>();
-            for(int i : nums){
-                current.add(i);
-            }
-            result.add(current);
-            return ;
-        }
-        for(int i = index; i <nums.length ; i++){
-            swap(nums, index, i);
-            permutate(result, index+1, nums);
-            swap(nums, index, i);
-        }
-    }
-
-    static void swap(int  [] nums, int i , int j){
+    static void swap(int [] nums, int i , int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
+    static void findPermutate(List<List<Integer>> result, int index, int [] nums){
+        if(index == nums.length){
+            List<Integer> possibles = new ArrayList<>();
+           for(int i : nums){
+             possibles.add(i);
+           }
+            result.add(possibles);
+            return;
+        }
+        for(int i = index; i < nums.length; i++){
+            swap(nums, i, index);
+            findPermutate(result, index + 1, nums);
+            swap(nums, i, index);
+        }
+    }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        permutate(result, 0, nums);
+        findPermutate(result, 0, nums);
         return result;
     }
 }
