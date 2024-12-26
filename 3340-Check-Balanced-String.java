9be56ec80712bc5faf6636e0078a81ae48eede53
Siranjeevi <164711059+Siranjeevi619@users.noreply.github.com>
2024-12-26 10:04:00 +0530

    [3340] [Time Beats: 99.29%] [Memory Beats: 73.24%] - LeetPush

--- 3340-Check-Balanced-String.java
+++ 3340-Check-Balanced-String.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isBalanced(String num) {
-        String [] nums = num.split("");
+        // String [] nums = num.split("");
         int oddCount = 0;
         int evenCount = 0;
         // int i = 0;
         // int j = 1;
-        int numsSize = nums.length ;
+        int numsSize = num.length() ;
         // while(i < numsSize  && j < numsSize ){
         //     evenCount += Integer.parseInt(nums[i]);
         //     oddCount += Integer.parseInt(nums[j]);
@@ -13,10 +13,11 @@ class Solution {
         //     j+=2;
         // }
         for(int i = 0; i < numsSize ; i++){
+            int current = num.charAt(i) - '0';
             if(i % 2 == 0){
-                evenCount +=Integer.parseInt(nums[i]);
+                evenCount += current;
             }else{
-                  oddCount +=Integer.parseInt(nums[i]);
+                  oddCount +=current;
             }
         }
         return oddCount == evenCount;
