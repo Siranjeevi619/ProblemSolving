4038c002daa75ecd5c36cc2794513dcecb98ddb9
Siranjeevi <164711059+Siranjeevi619@users.noreply.github.com>
2024-12-27 07:25:24 +0530

    [144] [Time Beats: 100.00%] [Memory Beats: 9.89%] - LeetPush

--- 144-Binary-Tree-Preorder-Traversal.java
+++ 144-Binary-Tree-Preorder-Traversal.java
@@ -15,14 +15,36 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List <Integer> ls = new ArrayList<>();
-        if(root == null){
-            return ls;
-        }
+        // List <Integer> ls = new ArrayList<>();
+        // if(root == null){
+        //     return ls;
+        // }
 
-        ls.add(root.val);
-        ls.addAll(preorderTraversal(root.left));
-        ls.addAll(preorderTraversal(root.right));
-        return ls;
+        // ls.add(root.val);
+        // ls.addAll(preorderTraversal(root.left));
+        // ls.addAll(preorderTraversal(root.right));
+        // return ls;
+
+
+          List<Integer> result = new ArrayList<>();
+        Stack <TreeNode> stack = new Stack<>();
+
+        if(root == null){
+            return result;
+        }
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode currentNode = stack.pop();
+            result.add(currentNode.val);
+            if(currentNode.right != null){
+                stack.push(currentNode.right);
+            } 
+            if(currentNode.left != null){
+                stack.push(currentNode.left);
+            }
+        }
+        return result;
+
+
     }
 }
