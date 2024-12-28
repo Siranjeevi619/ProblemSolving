9b47caab54d15214c6a2c90f7c260b7e20f698dc
Siranjeevi <164711059+Siranjeevi619@users.noreply.github.com>
2024-12-28 07:27:16 +0530

    [31] [Time Beats: 100.00%] [Memory Beats: 56.83%] - LeetPush

--- 31-Next-Permutation.java
+++ 31-Next-Permutation.java
@@ -1,36 +1,39 @@
 class Solution {
-    static void reverseArray(int [] nums, int i, int j){
-        while(i < j){
-            swap(nums, i,j);
+    static void reverseArray(int[] arr, int i, int j) {
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
             i++;
             j--;
         }
     }
 
-    static void swap(int [] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
     public void nextPermutation(int[] nums) {
         int numsSize = nums.length;
-        int index = -1 ;
-        for(int i=numsSize -2;i>=0;i--){
-            if(nums[i] < nums[i+1]){
+        int index = -1;
+
+        for (int i = numsSize - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
                 index = i;
                 break;
             }
         }
-        if(index == -1){
-            reverseArray(nums, 0,numsSize-1);
+
+        if (index == -1) {
+            reverseArray(nums, 0, numsSize - 1);
             return;
         }
-        for(int i=numsSize-1;i > index;i--){
-            if(nums[i] > nums[index]){
-                swap(nums, i, index);
+
+        for (int i = numsSize - 1; i > index; i--) {
+            if (nums[i] > nums[index]) {
+                int temp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = temp;
                 break;
             }
         }
-        reverseArray(nums, index+1, numsSize-1);
+
+        reverseArray(nums, index + 1, numsSize - 1);
     }
 }
