325e10c63f5a37af584de82112b6e299e12e1aa1
Siranjeevi <164711059+Siranjeevi619@users.noreply.github.com>
2024-12-30 22:55:24 +0530

    [1] [Time Beats: 32.23%] [Memory Beats: 48.98%] - LeetPush

--- 1-Two-Sum.java
+++ 1-Two-Sum.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int [] result = new int[2];
-        int numsSize = nums.length;
-        for(int i=0;i<numsSize;i++){
-          for(int j=i+1;j<numsSize;j++){
-            if(nums[i] + nums[j] == target){
-                    result[0] = i;
-                result[1] = j;
-                break;
+        int [] result = new int [2];
+        int arrSize = nums.length;
+        for(int  i = 0 ; i < arrSize ; i++ ){
+            for(int  j = i+1 ; j < arrSize ; j +=1){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
             }
-          }
         }
-        return result;
+        return new int[] {-1,-1};
     }
 }
