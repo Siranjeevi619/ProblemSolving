4c9f5e431f51df87d8cd2761a2beae505ea8a113
Siranjeevi <164711059+Siranjeevi619@users.noreply.github.com>
2025-01-07 10:20:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.java
@@ -0,0 +1,108 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    Node bottom;
+
+    Node(int x) {
+        data = x;
+        next = null;
+        bottom = null;
+    }
+}
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    // Function to flatten a linked list
+    Node flatten(Node root) {
+        // code here
+        ArrayList<Integer> elements = new ArrayList<>();
+        // Node head = root;
+        while(root != null){
+            Node child = root;
+            while(child !=null){
+                elements.add(child.data);
+                child = child.bottom;
+            }
+            root = root.next;
+        }
+        Collections.sort(elements);
+        Node result = new Node(elements.get(0));
+        Node current = result;
+        for(int i = 1 ; i < elements.size() ; i+=1){
+            Node newNode = new Node(elements.get(i));
+            current.bottom  = newNode;
+            current = newNode;
+            
+        }
+        return result;
+    }
+}
+
+//{ Driver Code Starts.
+
+public class GFG {
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.bottom;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // Consume the remaining newline
+
+        while (t-- > 0) {
+            String[] workArray = sc.nextLine().trim().split(" ");
+            int n = workArray.length;
+
+            Node head = null;
+            Node pre = null;
+
+            for (int i = 0; i < n; i++) {
+                int m = Integer.parseInt(workArray[i]);
+                int data = sc.nextInt();
+                Node temp = new Node(data);
+                if (head == null) {
+                    head = temp;
+                    pre = temp;
+                } else {
+                    pre.next = temp;
+                    pre = temp;
+                }
+
+                Node preB = temp;
+                for (int j = 0; j < m - 1; j++) {
+                    int tempData = sc.nextInt();
+                    Node tempB = new Node(tempData);
+                    preB.bottom = tempB;
+                    preB = tempB;
+                }
+            }
+
+            // Consume the remaining newline if there's any
+            if (sc.hasNextLine()) {
+                sc.nextLine();
+            }
+
+            Solution ob = new Solution();
+            Node root = ob.flatten(head);
+            printList(root);
+        
+System.out.println("~");
+}
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
