c7cf5d375b53afc599bcfff5a8618c28410cd65b
Siranjeevi <164711059+Siranjeevi619@users.noreply.github.com>
2025-02-04 07:33:15 +0530

    [1800] [Time Beats: 100.00%] [Memory Beats: 83.01%] - LeetPush

---
+++ 1800-Maximum-Ascending-Subarray-Sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int max = nums[0];
+        int sum = nums[0];
+        for(int i = 0; i < nums.length-1;i+=1){
+            if(nums[i] < nums[i+1]){
+                sum += nums[i+1];
+            }
+            else{
+                sum = nums[i+1];
+            }
+            max = Math.max(sum, max);
+        }
+        return max;
+
+    }
+}
