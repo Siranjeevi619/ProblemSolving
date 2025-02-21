1ecfa32783a297b031a6fe287006e74edbc941b6
Siranjeevi-Catom <164711059+Siranjeevi619@users.noreply.github.com>
2025-02-21 23:31:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+
+
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            new Solution().segregate0and1(arr);
+            for (int i = 0; i < array.size(); i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            // System.out.println("~");
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    void segregate0and1(int[] arr) {
+        // code here
+        Stack<Integer> zero = new Stack<>();
+        Stack<Integer> one = new Stack<>();
+        int size = arr.length;
+        for(int i= 0 ; i  < size ; i+=1){
+            if(arr[i] == 0){
+                zero.push(arr[i]);
+            }
+            else{
+                one.push(arr[i]);
+            }
+        }
+        size = 0;
+        while(!zero.isEmpty()){
+            arr[size++] = zero.pop();
+        }
+        while(!one.isEmpty()){
+            arr[size++] = one.pop();
+        }
+        
+    }
+}
