8fcee5a56bdf00f391cdafa6f37b97b4b089102a
Siranjeevi-Catom <164711059+Siranjeevi619@users.noreply.github.com>
2025-02-23 00:00:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.java
+++ Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.java
@@ -25,23 +25,32 @@ class GFG {
 class Solution {
     static int maxLength(String s) {
         // code here
-        int maxLength = 0;
-        Stack<Integer> st = new Stack<>();
-        st.push(-1);
-        for(int i =  0 ; i < s.length() ; i+=1){
-            if(s.charAt(i) =='('){
-                st.push(i);
-            }
-            else{
-                st.pop();
-                if(st.isEmpty()){
-                    st.push(i);
-                }
-                else{
-                    maxLength = Math.max(maxLength, i - st.peek());
+         Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int maxLen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+
+                // If we encounter a closing parenthesis, 
+                  // pop the stack
+                stack.pop();
+
+                // If stack is empty, push the current index 
+                // as a base for the next valid substring
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+
+                    // Update maxLength with the current length 
+                    // of the valid parentheses substring
+                    maxLen = Math.max(maxLen, i - stack.peek());
                 }
             }
         }
-        return maxLength;
+
+        return maxLen; 
     }
 }
