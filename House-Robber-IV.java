ea15e73e1cbb44804b9e84858a6c6cdecfbb09f3
Siranjeevi-Catom <164711059+Siranjeevi619@users.noreply.github.com>
2025-03-15 22:34:44 +0530

    [2560] [Time Beats: 90.05%] [Memory Beats: 96.52%] - LeetPush

---
+++ House-Robber-IV.java
@@ -0,0 +1,33 @@
+
+class Solution {
+    public int minCapability(int[] nums, int k) {
+        int left = Integer.MAX_VALUE, right = Integer.MIN_VALUE;
+        for (int num : nums) {
+            left = Math.min(left, num);
+            right = Math.max(right, num);
+        }
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canRob(nums, k, mid)) {
+                right = mid; 
+            } else {
+                left = mid + 1; 
+            }
+        }
+
+        return left;
+    }
+    private boolean canRob(int[] nums, int k, int mid) {
+        int count = 0;
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] <= mid) {
+                count++;
+                i++; 
+            }
+            i++; 
+        }
+        return count >= k;
+    }
+}
