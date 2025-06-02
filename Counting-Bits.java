9a420b10939aef55cc460a3c89e9819194bc1559
Siranjeevi-Catom <164711059+Siranjeevi619@users.noreply.github.com>
2025-06-02 22:55:03 +0530

    [338] [Time Beats: 9.18%] [Memory Beats: 7.21%] - LeetPush

--- Counting-Bits.java
+++ Counting-Bits.java
@@ -1,9 +1,22 @@
 class Solution {
-    public int[] countBits(int n) {
-        int [] result = new int[n+1];
-        for(int i = 1 ;i <= n ; i ++){
-            result[i] = result[i >>1] + (i&1);
+    static int check(int num){
+        StringBuilder sb = new StringBuilder();
+        while(num > 0){
+            sb.insert(0, num % 2);
+            num /= 2;
         }
-        return result;
+        String deci =  new String(sb);
+        int count = 0;
+        for(char ch : deci.toCharArray()) if(ch == '1') count+=1;
+        return count;
+    }
+    
+    public int[] countBits(int n) {
+        int [] res = new int[n+1];
+        res [0] = 0;
+        for(int i = 1; i <= n ; i+=1){
+            res[i] = check(i);
+        }
+        return res;
     }
 }
