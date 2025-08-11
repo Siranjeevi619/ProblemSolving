729a9fd33a28531b3c75586e8dbe62a16de4b9b9
Siranjeevi-Catom <164711059+Siranjeevi619@users.noreply.github.com>
2025-08-12 00:40:03 +0530

    Time: 3 ms (87.43%) | Memory: 60.9 MB (69.55%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,18 +1,18 @@
 class Solution {
     public int numOfSubarrays(int[] arr, int k, int threshold) {
         int n = arr.length;
-        if (k > n)
-            return 0;
-        int target = threshold * k;
         int count = 0;
-
-        for (int i = 0; i + k <= n; i++) {
-            int sum = 0;
-            for (int j = i; j < i + k; j++) {
-                sum += arr[j];
-            }
-            if (sum >= target) {
-                count++;
+        int avgSum = 0;
+        for(int i = 0 ; i < k ; i += 1){
+            avgSum += arr[i];
+        }
+        if(avgSum / k >= threshold){
+            count +=1;
+        }
+        for(int i = k ; i < arr.length;i+=1){
+            avgSum = avgSum + arr[i] - arr[i - k];
+            if(avgSum / k >= threshold){
+                count+=1;
             }
         }
         return count;
