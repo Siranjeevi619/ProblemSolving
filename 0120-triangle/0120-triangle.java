d044725f425a364d9a71dd2929d49e14acee0665
Siranjeevi-Catom <164711059+Siranjeevi619@users.noreply.github.com>
2025-09-25 07:09:20 +0530

    Time: 9 ms (11.07%), Space: 45.6 MB (42.22%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,21 +1,12 @@
 class Solution {
-    static int minPath(int i , int j, List<List<Integer>> triangle , int size, int [][] dp){
-        if(i == size - 1){
-            return triangle.get(size-1).get(j);
-        }
-        if(dp[i][j] !=  -1){
-            return dp[i][j];
-        }
-        int down = triangle.get(i).get(j) + minPath(i+1, j , triangle, size, dp);
-        int downRight = triangle.get(i).get(j) + minPath(i+1, j+1, triangle, size, dp);
-        return dp[i][j] = Math.min(down, downRight);
-    }
     public int minimumTotal(List<List<Integer>> triangle) {
-        int size = triangle.size();
-        int [][]dp = new int [size][size];
-        for(int [] d : dp){
-            Arrays.fill(d, -1);
+        int n = triangle.size();
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                triangle.get(i).set(j, triangle.get(i).get(j) + 
+                                    Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1)));
+            }
         }
-        return minPath(0, 0, triangle, size, dp);
+        return triangle.get(0).get(0);
     }
 }
