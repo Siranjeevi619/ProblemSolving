c53cd9d4b79f616a141fcf4b2cfdbc4ed84ba5b0
Siranjeevi-Catom <164711059+Siranjeevi619@users.noreply.github.com>
2025-10-01 22:24:02 +0530

    [48] [Time Beats: 100.00%] [Memory Beats: 90.12%] - LeetPush

--- Rotate-Image.java
+++ Rotate-Image.java
@@ -1,16 +1,23 @@
 class Solution {
-  public void rotate(int[][] matrix) {
-    for (int i = 0, j = matrix.length - 1; i < j; ++i, --j) {
-      int[] temp = matrix[i];
-      matrix[i] = matrix[j];
-      matrix[j] = temp;
-    }
 
-    for (int i = 0; i < matrix.length; ++i)
-      for (int j = i + 1; j < matrix.length; ++j) {
-        final int temp = matrix[i][j];
-        matrix[i][j] = matrix[j][i];
-        matrix[j][i] = temp;
-      }
-  }
+    public void rotate(int[][] matrix) {
+        int start = 0;
+        int end = matrix.length-1;
+        while(start < end){
+            int [] temp = matrix[start];
+            matrix[start] = matrix[end];
+            matrix[end] = temp;
+            start+=1;
+            end-=1;
+        } 
+        for(int i = 0 ; i < matrix.length;i+=1){
+            for(int j = i+1;j<matrix[0].length;j+=1)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+    }
 }
