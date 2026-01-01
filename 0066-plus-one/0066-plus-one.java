8eb6625f072445750891f94316696d947d532eec
Siranjeevi-Catom <164711059+Siranjeevi619@users.noreply.github.com>
2026-01-01 13:49:28 +0530

    Time: 0 ms (100%), Space: 43.6 MB (7.44%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-    int n = digits.length;
-    for (int i = n - 1; i >= 0; i--) {
-        if (digits[i] < 9) {
-            digits[i]++;
+       for(int i = digits.length-1 ; i >= 0 ; i--){
+        if(digits[i] < 9){
+            digits[i]+=1;
             return digits;
+
         }
-            digits[i] = 0;
-        }
-        
-        int[] newNumber = new int[n + 1];
-        newNumber[0] = 1;
-        return newNumber;
+        digits[i] = 0;
+       }
+       int [] newArr = new int[digits.length + 1];
+       newArr[0] = 1;
+       return newArr;
+
     }
 }
